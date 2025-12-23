3e0b0b26acf297313919517c91f7709dcb41c203
Mustafa KIZILTAY <mkiziltay@hotmail.com>
2025-12-24 00:19:18 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/bmi_calculators/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bmi_calculators
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/input_page/gender/gender_card.dart
@@ -0,0 +1,140 @@
+import 'package:bmi_calculator/input_page/card_title.dart';
+import 'package:bmi_calculator/input_page/gender/gender_arrow.dart';
+import 'package:bmi_calculator/input_page/gender/gender_circle.dart';
+import 'package:bmi_calculator/input_page/gender/gender_icon.dart';
+import 'package:bmi_calculator/input_page/gender/gender_styles.dart';
+import 'package:bmi_calculator/model/gender.dart';
+import 'package:bmi_calculator/widget_utils.dart' show screenAwareSize;
+import 'package:flutter/material.dart';
+
+class GenderCard extends StatefulWidget {
+  final Gender gender;
+  final ValueChanged<Gender> onChanged;
+
+  const GenderCard({
+    super.key,
+    this.gender = Gender.other,
+    required this.onChanged,
+  });
+
+  @override
+  _GenderCardState createState() => _GenderCardState();
+}
+
+class _GenderCardState extends State<GenderCard>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _arrowAnimationController;
+
+  @override
+  void initState() {
+    _arrowAnimationController = new AnimationController(
+      vsync: this,
+      lowerBound: -defaultGenderAngle,
+      upperBound: defaultGenderAngle,
+      value: genderAngles[widget.gender],
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _arrowAnimationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.only(
+        left: screenAwareSize(16.0, context),
+        right: screenAwareSize(4.0, context),
+        bottom: screenAwareSize(4.0, context),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          CardTitle("GENDER", subtitle: '',),
+          Padding(
+            padding: EdgeInsets.only(top: screenAwareSize(16.0, context)),
+            child: _drawMainStack(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _drawMainStack() {
+    return Container(
+      width: double.infinity,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: <Widget>[
+          _drawCircleIndicator(),
+          GenderIconTranslated(
+            key: Key('genderFemale'),
+            gender: Gender.female,
+            isSelected: widget.gender == Gender.female,
+          ),
+          GenderIconTranslated(
+            key: Key('genderOther'),
+            gender: Gender.other,
+            isSelected: widget.gender == Gender.other,
+          ),
+          GenderIconTranslated(
+            key: Key('genderMale'),
+            gender: Gender.male,
+            isSelected: widget.gender == Gender.male,
+          ),
+          _drawGestureDetector(),
+        ],
+      ),
+    );
+  }
+
+  Widget _drawCircleIndicator() {
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        GenderCircle(),
+        GenderArrow(key: Key('genderArrow'), listenable: _arrowAnimationController),
+      ],
+    );
+  }
+
+  _drawGestureDetector() {
+    return Positioned.fill(
+      child: TapHandler(
+        onGenderTapped: _setSelectedGender,
+      ),
+    );
+  }
+
+  void _setSelectedGender(Gender gender) {
+    widget.onChanged(gender);
+    _arrowAnimationController.animateTo(
+      genderAngles[gender] ?? 0.0,
+      duration: Duration(milliseconds: 150),
+    );
+  }
+}
+
+class TapHandler extends StatelessWidget {
+  final Function(Gender) onGenderTapped;
+
+  const TapHandler({Key? key, required this.onGenderTapped}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Expanded(
+            child: GestureDetector(onTap: () => onGenderTapped(Gender.female))),
+        Expanded(
+            child: GestureDetector(onTap: () => onGenderTapped(Gender.other))),
+        Expanded(
+            child: GestureDetector(onTap: () => onGenderTapped(Gender.male))),
+      ],
+    );
+  }
+}
